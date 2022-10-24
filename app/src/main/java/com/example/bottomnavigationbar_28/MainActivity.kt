1179b27d60dd2e4afb10ba4b9016a38201fa7c6d
Camarederic <camarederic@mail.ru>
2022-10-24 20:00:28 +0300

    Create a Fragments, navigation and menu

--- app/src/main/java/com/example/bottomnavigationbar_28/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationbar_28/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.bottomnavigationbar_28
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.bottomnavigationbar_28.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = Navigation.findNavController(this, R.id.nam_host_fragment)
+        setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
